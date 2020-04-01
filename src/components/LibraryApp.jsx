72b39fdf8f3d8fb1570d2f8957d6e369a545acb0
BBrownley <brendobrownley@yahoo.com>
2020-03-31 23:08:41 -0400

    Initial file setup

---
+++ src/components/LibraryApp.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class LibraryApp extends Component {
+  render() {
+    return (
+      <div>
+        <h1>LibraryApp</h1>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "normalize.css/normalize.css";
+import "./styles/dist/styles.min.css";
+import * as serviceWorker from "./serviceWorker";
+
+import LibraryApp from "./components/LibraryApp";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <LibraryApp />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
